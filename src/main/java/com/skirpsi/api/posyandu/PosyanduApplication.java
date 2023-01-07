4c5fc377dcae718727cf2b7d4dde40b79d7f3025
B-LAPTOP\user <tangguhdoesgaming@gmail.com>
2023-01-07 11:46:12 +0700

    Initialize Program

---
+++ src/main/java/com/skirpsi/api/posyandu/PosyanduApplication.java
@@ -0,0 +1,13 @@
+package com.skirpsi.api.posyandu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosyanduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosyanduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skirpsi/api/posyandu/PosyanduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skirpsi.api.posyandu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosyanduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
